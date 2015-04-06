d9590ab9069a6d11ad811f8a447f8182ab9f9073
Pablo Botta <pabloluisbotta@gmail.com>
2015-04-06 00:12:24 -0300

    Agregue calculo segun RG 3756

--- js/calculadora.js
+++ js/calculadora.js
@@ -50,7 +50,7 @@
             _c.value = minCuota;
         }
         document.getElementById("calcular").addEventListener("click", function (event) {
-            var c, d, n, i;
+            var c, d, n, i, rg3756;
             var info = document.getElementById("info");
             event.preventDefault();
             info.innerHTML = "";
@@ -58,6 +58,8 @@
             d = document.getElementById('d').value;
             n = document.getElementById('n').value;
             i = document.getElementById('i').value;
+            rg3756 = document.getElementById("rg3756").checked;
+            console.log(rg3756);
             if(esValido(c) && esValido(d) && esValido(n) && esValido(i)) {
                 var cantLetras = 0;
                 if(esLetra(c)) {
@@ -82,11 +84,20 @@
                         var aproximacion = -1;
                         var anterior = -1;
                         var cuotas = 0;
-                        resultado = "<table class='pure-table pure-table-horizontal pure-table-striped'><thead><tr><th>Cantidad de Cuotas</th><th>Valor de la cuota (aprox. en $)</th><th>Monto total del plan (aprox en $)</th></tr><tbody>";
-                        for (var j = 1; j < 12*50; j = j + 1) { //50 años
+                        var cuotaInicial = 0;
+                        resultado = resultado + "<table class='pure-table pure-table-horizontal pure-table-striped'><thead><tr>";
+                        resultado = resultado + (!rg3756 ? "" : "<th>Cuota inicial (7%)</th>");
+                        resultado = resultado + "<th>Cantidad de Cuotas</th><th>Valor de la cuota (aprox. en $)</th><th>Monto total del plan (aprox en $)</th></tr><tbody>";
+                        for (var j = 1; j < 12*10; j = j + 1) { 
                             resultado = resultado + "<tr>";
-                            var operacion = (d * i * Math.pow(1 + i, j) )  / ( Math.pow(1 + i, j) - 1 );
-                            //operacion = (55300 * 0.0135 * Math.pow((1 + 0.0135), j) )  / ( Math.pow((1 + 0.0135), j) - 1 )
+                            var operacion = 0;
+                            if(!rg3756) {
+                                operacion = (d * i * Math.pow(1 + i, j) )  / ( Math.pow(1 + i, j) - 1 );
+                            } else {
+                                operacion = (d * i * 0.93 * Math.pow(1 + i, j) )  / ( Math.pow(1 + i, j - 1) - 1 );
+                                cuotaInicial = d * 0.07 * (1 + i);
+                            }
+                                
                             if(operacion < c && aproximacion === -1) {
                                 if(Math.abs(operacion - c) < Math.abs(anterior - c)) {
                                     aproximacion = operacion;
@@ -97,20 +108,36 @@
                                     cuotas = j - 1;
                                 }
                             }
-                            resultado = resultado + "<td>" + lpad(j,3) +"</td><td class='cell-number'> " + operacion.toFixed(2) + "</td><td class='cell-number'>" + (operacion * j).toFixed(2) + "</td></tr>";
+                            if(rg3756) {
+                                resultado = resultado + "<td>" + cuotaInicial.toFixed(2) + "</td>";
+                            } else {
+                                cuotaInicial = aproximacion;
+                            }
+                            resultado = resultado + "<td>" + lpad(j, 3) +"</td><td class='cell-number'> " + operacion.toFixed(2) + "</td><td class='cell-number'>" + (operacion * (j - 1) + cuotaInicial).toFixed(2) + "</td></tr>";
                             anterior = operacion;
                         }
                         resultado = resultado + "</tbody></table>";
-                        resultado = "<h2>Plan recomendado</h2><p><strong>Cuotas: " + cuotas +", Valor de la cuota: $ " + aproximacion.toFixed(2) + " - Monto Total del plan: $ " + (cuotas * aproximacion).toFixed(2) + "</strong></p><p>Pasos de la aproximación:</p>" + resultado;
-                        
+                        resultado = "<h2>Plan recomendado</h2><p><strong>Cuotas: " + cuotas + " - Valor de la cuota: $ " + aproximacion.toFixed(2) + " - Monto Total del plan: $ " + ((cuotas - 1) * aproximacion + cuotaInicial).toFixed(2) + "</strong></p><p>Pasos de la aproximación:</p>" + resultado;
                     } else {
                         if(esLetra(c)) {
                             //Hay que averiguar la cuota;
                             n = parseFloat(n);
                             d = parseFloat(d);
                             i = parseFloat(i);
-                            var operacion = (d * i * Math.pow(1 + i, n) )  / ( Math.pow(1 + i, n) - 1 );
-                            resultado = "<p><strong>El valor de la cuota es $ " + operacion.toFixed(2) + "</strong></p>";
+                            var operacion = 0;
+                            if(!rg3756) {
+                                operacion = (d * i * Math.pow(1 + i, n) )  / ( Math.pow(1 + i, n) - 1 );
+                            } else {
+                                operacion = (d * i * 0.93 * Math.pow(1 + i, n) )  / ( Math.pow(1 + i, n - 1) - 1 );
+                                var cuotaInicial = d * 0.07 * (1 + i);
+                            }
+                            if(cuotaInicial) {
+                                resultado = resultado + "<p><strong>La cuota inicial es $ " + cuotaInicial.toFixed(2) + "</strong></p>";
+                            } else {
+                                cuotaInicial = operacion;
+                            }
+                            resultado = resultado + "<p><strong>El valor de la cuota es $ " + operacion.toFixed(2) + "</strong></p>";
+                            resultado = resultado + "<p><strong>Monto total del plan $ " + (cuotaInicial + (n - 1) * operacion).toFixed(2) + "</strong></p>";
                         }
                     }
                     log.innerHTML = resultado;
@@ -123,6 +150,14 @@
                 }
             } else {
                 info.innerHTML = "<p class='error'>Algunos de los parámetros no tiene un valor válido.<p>";
+            }
+        });
+        document.getElementById("rg3756").addEventListener("change", function (event) {
+            var txtInteres = document.getElementById("i");
+            if(this.checked) {
+                txtInteres.value = 0.019;
+            } else {
+                txtInteres.value = 0.0135;
             }
         });
     });
